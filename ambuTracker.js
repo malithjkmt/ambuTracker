52ddcf8a83b8dddf40409dc61a35e120a7fbd402
Malith <malit.tilak@gmail.com>
2016-03-12 08:18:41 +0530

    Add dburles google maps package and load a basic map using this API

--- ambuTracker.js
+++ ambuTracker.js
@@ -1,11 +1,20 @@
 if (Meteor.isClient) {
+    Meteor.startup(function() {
+        GoogleMaps.load();
+    });
 
-}
+    Template.map.helpers({
+        myMapOptions: function() {
+            // Make sure the maps API has loaded
+            if (GoogleMaps.loaded()) {
+                // Map initialization options
+                return {
+                    center: new google.maps.LatLng(6.523358, 80.114081),
+                    zoom: 12
+                };
+            }
+        }
+    });
 
 
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
