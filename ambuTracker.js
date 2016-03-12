5372b3f5256b8514c3257aa3ae86f8bf37b0c164
Malith <malit.tilak@gmail.com>
2016-03-12 11:32:39 +0530

    Make the marker and the map view reactive usin "autorun"

--- ambuTracker.js
+++ ambuTracker.js
@@ -1,4 +1,5 @@
 Markers = new Mongo.Collection('markers');
+var MAP_ZOOM = 14;
 
 if (Meteor.isClient) {
     Meteor.startup(function() {
@@ -16,29 +17,42 @@ if (Meteor.isClient) {
             if (GoogleMaps.loaded() && latLng) {
                 return {
                     center: new google.maps.LatLng(latLng.lat, latLng.lng),
-                    zoom: 12
+                    zoom: MAP_ZOOM
                 };
             }
         }
     });
 
-    Template.body.onCreated(function() {
 
-        // Once the map is ready on the screen
+
+    Template.map.onCreated(function() {
+        var self = this;
+
         GoogleMaps.ready('myMap', function(map) {
-            //get current position of client
-            var latLng = Geolocation.latLng();
+            var marker;
 
-            var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(latLng.lat, latLng.lng),
-                map: map.instance
+            // Create and move the marker when latLng changes.
+            self.autorun(function() {
+                var latLng = Geolocation.latLng();
+                if (! latLng)
+                    return;
+
+                // If the marker doesn't yet exist, create it.
+                if (! marker) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(latLng.lat, latLng.lng),
+                        map: map.instance
+                    });
+                }
+                // The marker already exists, so we'll just change its position.
+                else {
+                    marker.setPosition(latLng);
+                }
+
+                // Center and zoom the map view onto the current position.
+                map.instance.setCenter(marker.getPosition());
+                map.instance.setZoom(MAP_ZOOM);
             });
-/*
-            Markers.insert({
-                lat: currentPosition.latitude,
-                lng: currentPosition.longitude
-            })*/
-
         });
     });
 
