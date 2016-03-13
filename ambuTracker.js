d9a99d2878df46403e45327811611e1aa2d7ed2e
Malith <malit.tilak@gmail.com>
2016-03-13 08:36:41 +0530

    Assign unique marker for each user. ToDO:- 1. Remove logged out users 2. Improve security(adding server methods and removing insecure and auto publish) 3. Implement basic user requirements 4. UI

--- ambuTracker.js
+++ ambuTracker.js
@@ -60,33 +60,45 @@ if (Meteor.isClient) {
 
         GoogleMaps.ready('myMap', function(map) {
 
-            var position;
-            var positions = [];
+
+            positions = [];
+
 
             // Create and move the position when latLng changes.
             self.autorun(function() {
+                console.log('positions '+positions);
 
                 //####### Setup markers########
 
+                // Get current markers of all users on the map
 
                 // get all current markers from Collection
                 var markerCursor = Positions.find({});
 
-                // Update position of each user
+
                 markerCursor.forEach(function(pos) {
                     console.log(pos.position);
+                    console.log(pos.userId);
+                    console.log(pos._id);
 
-                    if(!positions[pos._id]){
-                        positions[pos._id] = new google.maps.Marker({
+                    // if the marker of the user has never been added to the map
+                    if(!positions[pos.userId]){
+                        console.log('added new marker for '+pos.username);
+                        positions[pos.userId] = new google.maps.Marker({
                             position: new google.maps.LatLng(pos.position.lat, pos.position.lng),
                             map: map.instance,
                             label: pos.username,
                             title: pos.username
                         });
                     }
+
+                  /*  if(Meteor.user[pos._id]){
+                        console.log('user ' + pos.username+ ' logged in');
+                    }*/
+                    // if the marker of that user already been added to the map
                     else{
-                        console.log('inside else');
-                         positions[pos._id].setPosition(pos.position);
+                         console.log('updating marker for '+pos.username);
+                         positions[pos.userId].setPosition(pos.position);
                     }
                 });
 
@@ -95,18 +107,20 @@ if (Meteor.isClient) {
                 if(Meteor.user()) {
                     var userId = Meteor.userId();
                     //update Collection
-                    console.log(userId +  " user is being updated because his position changed!!!");
+
                     var latLng = Geolocation.latLng();
                     if (! latLng)
-                        console.log('returned!!!!');
                         return;
+
                     // get the _id of this user
                     var id = Positions.findOne({userId: userId})._id;
 
                     // Update the collection
                     Positions.update({_id: id}, {$set: {position: {lat:latLng.lat, lng:latLng.lng}}});
-
+                    console.log(userId +  " my position changed and updated the Mongo");
                 }
+
+
                 // Center and zoom the map view onto the current position.
                // map.instance.setCenter(position.getPosition());
                // map.instance.setZoom(MAP_ZOOM)
