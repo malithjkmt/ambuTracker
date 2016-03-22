340dfdba2498cd0cbc0bb38fb553b984569cc7fa
Malith <malit.tilak@gmail.com>
2016-03-23 04:58:30 +0530

    Add custom control to the Google map  this is to center to a location. (now location is hard coded, after applying Geolocation, this will be the actual current location)

--- client/lib/google_maps.js
+++ client/lib/google_maps.js
@@ -7,6 +7,10 @@ gmaps = {
     // google markers objects
     markers: [],
 
+    Moratuwa : {lat: 6.795086, lng: 79.900783},
+
+
+
     // google lat lng objects
     latLngs: [],
 
@@ -18,7 +22,7 @@ gmaps = {
         var gLatLng = new google.maps.LatLng(marker.lat, marker.lng);
         var gMarker = new google.maps.Marker({
             position: gLatLng,
-            map: this.map,
+            map: gmaps.map,
             title: marker.title,
             // animation: google.maps.Animation.DROP,
             icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
@@ -41,12 +45,16 @@ gmaps = {
 
     // check if a marker already exists
     markerExists: function(key, val) {
-        _.each(this.markers, function(storedMarker) {
+        _.each(gmaps.markers, function(storedMarker) {
             if (storedMarker[key] == val)
                 return true;
         });
         return false;
     },
+
+
+
+
 
     // initialize the map
     initialize: function() {
@@ -69,7 +77,6 @@ gmaps = {
             streetViewControl: true,
             rotateControl: true,
             fullscreenControl: true,
-
             mapTypeControl: true,
             mapTypeControlOptions: {
                 style: google.maps.MapTypeControlStyle.DEFAULT,
@@ -82,14 +89,69 @@ gmaps = {
 
         };
 
-        this.map = new google.maps.Map(
+        gmaps.map = new google.maps.Map(
             document.getElementById('map-canvas'),
             mapOptions
         );
 
+        // Create the DIV to hold the control and call the CenterControl()
+        // constructor passing in this DIV.
+        var centerControlDiv = document.createElement('div');
+        new gmaps.CenterControl(centerControlDiv, gmaps.map);
+
+        centerControlDiv.index = 1;
+        gmaps.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv);
 
         // global flag saying we intialized already
         Session.set('map', true);
+
+
+
+    },
+
+    CenterControl: function(controlDiv, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '100%';
+        controlUI.style.width = '50px';
+        controlUI.style.height = '50px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to recenter the map';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+
+        // Set background Image
+        controlUI.style.backgroundImage = "url('img/loc_black_30.png')";
+        controlUI.style.backgroundRepeat = 'no-repeat';
+        controlUI.style.backgroundPosition = 'center';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: simply set the map to Moratuwa.
+        controlUI.addEventListener('click', function() {
+            gmaps.map.setCenter(gmaps.Moratuwa)
+        });
+
+        controlUI.addEventListener('mouseover', function() {
+            controlUI.style.backgroundImage = "url('img/loc2_blue_30.png')";
+        });
+
+        controlUI.addEventListener('mouseout', function() {
+            controlUI.style.backgroundImage = "url('img/loc_black_30.png')";
+        });
+
     }
+
+
 
 }
